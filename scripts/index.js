917d8b0f1af6a4754ce1107a2f377186b2f7ca4c
HASANALI117 <hassanali61911@gmail.com>
2024-11-27 18:39:40 +0300

    Refactor navigation structure and styles; add close and menu button IDs for improved functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,45 @@
 // Form
-const firstNameInput = document.getElementById("fname");
-const lastNameInput = document.getElementById("lname");
-const emailInput = document.getElementById("email");
-const mobileNoInput = document.getElementById("mobile");
-const button = document.getElementById("submit-button");
-const firstName = document.getElementById("first-name");
-const messageContainer = document.getElementById("message-container");
+// const firstNameInput = document.getElementById("fname");
+// const lastNameInput = document.getElementById("lname");
+// const emailInput = document.getElementById("email");
+// const mobileNoInput = document.getElementById("mobile");
+// const button = document.getElementById("submit-button");
+// const firstName = document.getElementById("first-name");
+// const messageContainer = document.getElementById("message-container");
 
-const formData = {
-  firstName: firstNameInput.value,
-  lastName: lastNameInput.value,
-  email: emailInput.value,
-  mobileNo: mobileNoInput.value,
-};
+// const formData = {
+//   firstName: firstNameInput.value,
+//   lastName: lastNameInput.value,
+//   email: emailInput.value,
+//   mobileNo: mobileNoInput.value,
+// };
 
-button.addEventListener("click", (e) => {
-  e.preventDefault();
+// button.addEventListener("click", (e) => {
+//   e.preventDefault();
 
-  let errorMessage = "";
+//   let errorMessage = "";
 
-  if (formData.firstName.length < 3) {
-    errorMessage += "First Name must have at least 3 characters. ";
-  }
+//   if (formData.firstName.length < 3) {
+//     errorMessage += "First Name must have at least 3 characters. ";
+//   }
 
-  if (formData.lastName.length < 3) {
-    errorMessage += "Last Name must have at least 3 characters. ";
-  }
+//   if (formData.lastName.length < 3) {
+//     errorMessage += "Last Name must have at least 3 characters. ";
+//   }
 
-  if (formData.mobileNo.length != 8) {
-    errorMessage += "Mobile No must contain exactly 8 digits. ";
-  }
+//   if (formData.mobileNo.length != 8) {
+//     errorMessage += "Mobile No must contain exactly 8 digits. ";
+//   }
 
-  if (errorMessage) {
-    messageContainer.innerHTML = `<p style="color: red;">${errorMessage}</p>`;
-  } else {
-    messageContainer.innerHTML = `<p style="color: green;">Form submitted successfully!</p>`;
-    console.log(formData);
+//   if (errorMessage) {
+//     messageContainer.innerHTML = `<p style="color: red;">${errorMessage}</p>`;
+//   } else {
+//     messageContainer.innerHTML = `<p style="color: green;">Form submitted successfully!</p>`;
+//     console.log(formData);
 
-    firstName.innerHTML = formData.firstName;
-  }
-});
+//     firstName.innerHTML = formData.firstName;
+//   }
+// });
 
 // Slider
 // const sliderTrack = document.querySelector(".slider-track");
@@ -58,8 +58,8 @@ button.addEventListener("click", (e) => {
 
 // Navbar
 const navLinks = document.getElementById("nav-links");
-const menuButton = document.querySelector(".fa-bars");
-const closeButton = document.querySelector(".fa-x");
+const menuButton = document.getElementById("menu-btn");
+const closeButton = document.getElementById("close-btn");
 
 menuButton.addEventListener("click", () => {
   navLinks.classList.add("show");
