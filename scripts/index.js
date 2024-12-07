cf574ae0dbe4fcd9613de25fa08e977da4391c9e
HASANALI117 <hassanali61911@gmail.com>
2024-12-07 22:13:43 +0300

    Fix reference-row null error on other pages

--- scripts/index.js
+++ scripts/index.js
@@ -88,195 +88,197 @@ if (storedData) {
 
 // Image References
 document.addEventListener("DOMContentLoaded", function () {
-  const imageReferences = [
-    {
-      src: "../images/home/img-1.jpg",
-      link: "https://www.polytechnic.bh/",
-      title: "Banner",
-    },
-    {
-      src: "../images/home/education.jpg",
-      link: "https://www.freepik.com/",
-      title: "Education",
-    },
-    {
-      src: "../images/home/class-3.jpg",
-      link: "https://www.polytechnic.bh/",
-      title: "Class",
-    },
-    {
-      src: "../images/home/group.jpg",
-      link: "https://www.freepik.com/",
-      title: "Group",
-    },
-    {
-      src: "../images/home/acadmic-advising.jpg",
-      link: "https://www.polytechnic.bh/",
-      title: "Acadmic-Advising",
-    },
-    {
-      src: "../images/home/student-info-centre.jpg",
-      link: "https://www.polytechnic.bh/",
-      title: "Student Info Centre",
-    },
-    {
-      src: "../images/home/career-and-employment-centre.png",
-      link: "https://www.polytechnic.bh/",
-      title: "Career & Employment Centre",
-    },
-    {
-      src: "../images/home/Library Learning Centre.jpg",
-      link: "https://www.polytechnic.bh/",
-      title: "Library Learning Centre",
-    },
-    {
-      src: "../images/home/teacher-1.jpg",
-      link: "https://www.polytechnic.bh/",
-      title: "Teacher-1",
-    },
-    {
-      src: "../images/home/teacher-2.jpg",
-      link: "https://www.polytechnic.bh/",
-      title: "Teacher-2",
-    },
-    {
-      src: "../images/home/teacher-3.jpg",
-      link: "https://www.polytechnic.bh/",
-      title: "Teacher-3",
-    },
-    {
-      src: "../images/home/teacher-4.jpg",
-      link: "https://www.polytechnic.bh/",
-      title: "Teacher-4",
-    },
-    {
-      src: "../images/home/teacher-5.jpg",
-      link: "https://www.polytechnic.bh/",
-      title: "Teacher-5",
-    },
-    {
-      src: "../images/home/teacher-6.jpg",
-      link: "https://www.polytechnic.bh/",
-      title: "Teacher-6",
-    },
-    {
-      src: "../images/home/student-1.JPG",
-      link: "https://www.polytechnic.bh/",
-      title: "Student-1",
-    },
-    {
-      src: "../images/home/student-2.JPG",
-      link: "https://www.polytechnic.bh/",
-      title: "Student-2",
-    },
-    {
-      src: "../images/img-4.jpg",
-      link: "https://www.polytechnic.bh/",
-      title: "Campus-1",
-    },
-    {
-      src: "../images/img-3.jpg",
-      link: "https://www.polytechnic.bh/",
-      title: "Campus-2",
-    },
-    {
-      src: "../images/academics/masters-1.jpg",
-      link: "https://www.freepik.com/",
-      title: "Masters-1",
-    },
-    {
-      src: "../images/academics/masters-2.jpg",
-      link: "https://www.freepik.com/",
-      title: "Masters-2",
-    },
-    {
-      src: "../images/academics/masters-3.jpeg",
-      link: "https://www.freepik.com/",
-      title: "Masters-3",
-    },
-    {
-      src: "../images/academics/masters-4.jpg",
-      link: "https://www.freepik.com/",
-      title: "Masters-4",
-    },
-    {
-      src: "../images/academics/masters-5.jpg",
-      link: "https://www.freepik.com/",
-      title: "Masters-5",
-    },
-    {
-      src: "../images/academics/bachelor-1.jpg",
-      link: "https://www.freepik.com/",
-      title: "Bachelor-1",
-    },
-    {
-      src: "../images/academics/bachelor-2.jpEg",
-      link: "https://www.freepik.com/",
-      title: "Bachelor-2",
-    },
-    {
-      src: "../images/academics/bachelor-3.jpg",
-      link: "https://www.freepik.com/",
-      title: "Bachelor-3",
-    },
-    {
-      src: "../images/academics/bachelor-4.jpg",
-      link: "https://www.freepik.com/",
-      title: "Bachelor-4",
-    },
-    {
-      src: "../images/academics/bachelor-5.jpg",
-      link: "https://www.polytechnic.bh/",
-      title: "Bachelor-5",
-    },
-    {
-      src: "../images/academics/topup-1.jpg",
-      link: "https://www.freepik.com/",
-      title: "Topup-1",
-    },
-    {
-      src: "../images/academics/topup-2.jpg",
-      link: "https://www.freepik.com/",
-      title: "Topup-2",
-    },
-    {
-      src: "../images/academics/topup-3.jpg",
-      link: "https://www.freepik.com/",
-      title: "Topup-3",
-    },
-    {
-      src: "../images/academics/topup-4.jpg",
-      link: "https://www.freepik.com/",
-      title: "Topup-4",
-    },
-    {
-      src: "../images/academics/topup-5.jpg",
-      link: "https://www.freepik.com/",
-      title: "Topup-5",
-    },
-  ];
-
   const referencesSection = document.getElementById("references-row");
 
-  imageReferences.forEach((image) => {
-    const linkElement = document.createElement("a");
-    linkElement.href = image.link;
+  if (referencesSection) {
+    const imageReferences = [
+      {
+        src: "../images/home/img-1.jpg",
+        link: "https://www.polytechnic.bh/",
+        title: "Banner",
+      },
+      {
+        src: "../images/home/education.jpg",
+        link: "https://www.freepik.com/",
+        title: "Education",
+      },
+      {
+        src: "../images/home/class-3.jpg",
+        link: "https://www.polytechnic.bh/",
+        title: "Class",
+      },
+      {
+        src: "../images/home/group.jpg",
+        link: "https://www.freepik.com/",
+        title: "Group",
+      },
+      {
+        src: "../images/home/acadmic-advising.jpg",
+        link: "https://www.polytechnic.bh/",
+        title: "Acadmic-Advising",
+      },
+      {
+        src: "../images/home/student-info-centre.jpg",
+        link: "https://www.polytechnic.bh/",
+        title: "Student Info Centre",
+      },
+      {
+        src: "../images/home/career-and-employment-centre.png",
+        link: "https://www.polytechnic.bh/",
+        title: "Career & Employment Centre",
+      },
+      {
+        src: "../images/home/Library Learning Centre.jpg",
+        link: "https://www.polytechnic.bh/",
+        title: "Library Learning Centre",
+      },
+      {
+        src: "../images/home/teacher-1.jpg",
+        link: "https://www.polytechnic.bh/",
+        title: "Teacher-1",
+      },
+      {
+        src: "../images/home/teacher-2.jpg",
+        link: "https://www.polytechnic.bh/",
+        title: "Teacher-2",
+      },
+      {
+        src: "../images/home/teacher-3.jpg",
+        link: "https://www.polytechnic.bh/",
+        title: "Teacher-3",
+      },
+      {
+        src: "../images/home/teacher-4.jpg",
+        link: "https://www.polytechnic.bh/",
+        title: "Teacher-4",
+      },
+      {
+        src: "../images/home/teacher-5.jpg",
+        link: "https://www.polytechnic.bh/",
+        title: "Teacher-5",
+      },
+      {
+        src: "../images/home/teacher-6.jpg",
+        link: "https://www.polytechnic.bh/",
+        title: "Teacher-6",
+      },
+      {
+        src: "../images/home/student-1.JPG",
+        link: "https://www.polytechnic.bh/",
+        title: "Student-1",
+      },
+      {
+        src: "../images/home/student-2.JPG",
+        link: "https://www.polytechnic.bh/",
+        title: "Student-2",
+      },
+      {
+        src: "../images/img-4.jpg",
+        link: "https://www.polytechnic.bh/",
+        title: "Campus-1",
+      },
+      {
+        src: "../images/img-3.jpg",
+        link: "https://www.polytechnic.bh/",
+        title: "Campus-2",
+      },
+      {
+        src: "../images/academics/masters-1.jpg",
+        link: "https://www.freepik.com/",
+        title: "Masters-1",
+      },
+      {
+        src: "../images/academics/masters-2.jpg",
+        link: "https://www.freepik.com/",
+        title: "Masters-2",
+      },
+      {
+        src: "../images/academics/masters-3.jpeg",
+        link: "https://www.freepik.com/",
+        title: "Masters-3",
+      },
+      {
+        src: "../images/academics/masters-4.jpg",
+        link: "https://www.freepik.com/",
+        title: "Masters-4",
+      },
+      {
+        src: "../images/academics/masters-5.jpg",
+        link: "https://www.freepik.com/",
+        title: "Masters-5",
+      },
+      {
+        src: "../images/academics/bachelor-1.jpg",
+        link: "https://www.freepik.com/",
+        title: "Bachelor-1",
+      },
+      {
+        src: "../images/academics/bachelor-2.jpEg",
+        link: "https://www.freepik.com/",
+        title: "Bachelor-2",
+      },
+      {
+        src: "../images/academics/bachelor-3.jpg",
+        link: "https://www.freepik.com/",
+        title: "Bachelor-3",
+      },
+      {
+        src: "../images/academics/bachelor-4.jpg",
+        link: "https://www.freepik.com/",
+        title: "Bachelor-4",
+      },
+      {
+        src: "../images/academics/bachelor-5.jpg",
+        link: "https://www.polytechnic.bh/",
+        title: "Bachelor-5",
+      },
+      {
+        src: "../images/academics/topup-1.jpg",
+        link: "https://www.freepik.com/",
+        title: "Topup-1",
+      },
+      {
+        src: "../images/academics/topup-2.jpg",
+        link: "https://www.freepik.com/",
+        title: "Topup-2",
+      },
+      {
+        src: "../images/academics/topup-3.jpg",
+        link: "https://www.freepik.com/",
+        title: "Topup-3",
+      },
+      {
+        src: "../images/academics/topup-4.jpg",
+        link: "https://www.freepik.com/",
+        title: "Topup-4",
+      },
+      {
+        src: "../images/academics/topup-5.jpg",
+        link: "https://www.freepik.com/",
+        title: "Topup-5",
+      },
+    ];
 
-    const imgRefDiv = document.createElement("div");
+    imageReferences.forEach((image) => {
+      const linkElement = document.createElement("a");
+      linkElement.href = image.link;
 
-    imgRefDiv.classList.add("img-ref");
+      const imgRefDiv = document.createElement("div");
 
-    const imgElement = document.createElement("img");
-    imgElement.src = image.src;
+      imgRefDiv.classList.add("img-ref");
 
-    const titleParagraph = document.createElement("p");
-    titleParagraph.textContent = image.title;
+      const imgElement = document.createElement("img");
+      imgElement.src = image.src;
 
-    imgRefDiv.appendChild(imgElement);
-    imgRefDiv.appendChild(titleParagraph);
+      const titleParagraph = document.createElement("p");
+      titleParagraph.textContent = image.title;
 
-    linkElement.appendChild(imgRefDiv);
+      imgRefDiv.appendChild(imgElement);
+      imgRefDiv.appendChild(titleParagraph);
 
-    referencesSection.appendChild(linkElement);
-  });
+      linkElement.appendChild(imgRefDiv);
+
+      referencesSection.appendChild(linkElement);
+    });
+  }
 });
